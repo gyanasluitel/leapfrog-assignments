9f3046d4261a2ccdfe1d130a01f41015b867a6ae
gyanasluitel <gyanasluitel@gmail.com>
2022-01-10 10:03:00 +0545

    added: gameOver state

--- final-project-archero/js/gameStates.js
+++ final-project-archero/js/gameStates.js
@@ -32,3 +32,29 @@ class GetReady {
     }
   };
 }
+
+class GameOver {
+  constructor(ctx) {
+    this.ctx = ctx;
+    this.playButtonX = CANVAS_WIDTH / 3;
+    this.playButtonY = CANVAS_HEIGHT / 4;
+    this.playButtonWidth = 150;
+    this.playButtonHeight = 50;
+  }
+
+  draw = () => {
+    if (gameStates.current === gameStates.gameOver) {
+      // Draw Game Start Background Image
+      this.ctx.drawImage(gameStartBgImage, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+      // Draw Play Button
+      this.ctx.drawImage(
+        playButtonImage,
+        this.playButtonX,
+        this.playButtonY,
+        this.playButtonWidth,
+        this.playButtonHeight
+      );
+    }
+  };
+}
