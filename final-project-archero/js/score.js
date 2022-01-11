94cb5c853cd59d155e450c2f8f45d11483cff3f0
gyanasluitel <gyanasluitel@gmail.com>
2022-01-11 13:33:36 +0545

    added: current level indicator

--- final-project-archero/js/score.js
+++ final-project-archero/js/score.js
@@ -3,41 +3,53 @@ class Score {
     this.ctx = ctx;
     this.coinsCollectedPerGame = 0;
     this.totalCoinsCollected = localStorage.getItem('arcHeroScore') || 0;
-    this.scoreBoardWidth = 50;
+    this.scoreBoardWidth = 60;
     this.scoreBoardHeight = 20;
 
     this.coinWidth = 20;
     this.coinHeight = 20;
 
-    this.x = CANVAS_WIDTH - 60;
+    this.x = CANVAS_WIDTH - 70;
     this.y = 20;
   }
 
   draw = () => {
-    if (gameStates.current === gameStates.gameRunning) {
-      this.ctx.strokeStyle = 'gray';
-
-      // Draw Score Board
-      this.ctx.strokeRect(
-        this.x,
-        this.y,
-        this.scoreBoardWidth,
-        this.scoreBoardHeight
-      );
-
-      // Draw Coin Image
-      this.ctx.drawImage(
-        coinImage,
-        this.x,
-        this.y,
-        this.coinWidth,
-        this.coinHeight
-      );
-
-      // Draw Score
-      this.ctx.fillStyle = 'white';
-      this.ctx.font = 'bold 20px Arial';
-      this.ctx.fillText(this.totalCoinsCollected, this.x + 25, this.y + 17);
+    if (
+      gameStates.current !== gameStates.getReady &&
+      gameStates.current !== gameStates.gameOver
+    ) {
+      this.drawCoinScore();
+      this.drawCurrentLevel();
     }
+  };
+
+  drawCoinScore = () => {
+    // this.ctx.strokeStyle = 'gray';
+
+    // // Draw Score Board
+    // this.ctx.strokeRect(
+    //   this.x,
+    //   this.y,
+    //   this.scoreBoardWidth,
+    //   this.scoreBoardHeight
+    // );
+    this.ctx.drawImage(
+      coinImage,
+      this.x,
+      this.y,
+      this.coinWidth,
+      this.coinHeight
+    );
+
+    // Draw Score
+    this.ctx.fillStyle = 'white';
+    this.ctx.font = 'bold 18px Arial';
+    this.ctx.fillText(this.totalCoinsCollected, this.x + 20, this.y + 15);
+  };
+
+  drawCurrentLevel = () => {
+    this.ctx.fill = 'white';
+    this.ctx.font = 'bold 25px Arial';
+    this.ctx.fillText(gameStates.currentLevel, CANVAS_WIDTH / 2 - 5, 45);
   };
 }
