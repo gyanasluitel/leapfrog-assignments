baf0c29351ce8d79192539adfb1c85cf7c1309b1
gyanasluitel <gyanasluitel@gmail.com>
2022-01-11 13:31:22 +0545

    added: current level indicator

--- final-project-archero/js/background.js
+++ final-project-archero/js/background.js
@@ -1,6 +1,11 @@
 class Background {
   constructor(ctx) {
     this.ctx = ctx;
+
+    this.gateLevelImageWidth = 50;
+    this.gateLevelImageHeight = 50;
+    this.gateLevelImageX = CANVAS_WIDTH / 2 - 22;
+    this.gateLevelImageY = 10;
   }
 
   draw = () => {
@@ -9,6 +14,15 @@ class Background {
       gameStates.current !== gameStates.gameOver
     ) {
       this.ctx.drawImage(backgroundImage, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+      // Draw Level Indicator Gate
+      this.ctx.drawImage(
+        gateLevelImage,
+        this.gateLevelImageX,
+        this.gateLevelImageY,
+        this.gateLevelImageWidth,
+        this.gateLevelImageHeight
+      );
     }
   };
 }
